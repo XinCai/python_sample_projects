e9f83b0fa08dbf43baf3d094d884cca12b02b1f2
CaiXin <caixinemail@gmail.com>
2020-05-24 10:34:09 +1000

    fetch abs data

--- request_html.py
+++ request_html.py
@@ -1,16 +1,18 @@
 from requests_html import HTMLSession
+from bs4 import BeautifulSoup
 
 session = HTMLSession()
 
 r = session.get('https://www.abs.gov.au/')
 #r = session.get('https://www.abs.gov.au/AUSSTATS/abs@.nsf/Web+Pages/Population+Clock?opendocument&ref=HPKI')
 
+print(r.html)
 #print(r.html.html)
 #print(r.html.links)
 #print(r.html.absolute_links)
 #print(r.html.xpath("//*[@id="KeyIndicators"]/a[1]/text()"))
 #print(r.html.find('div#KeyIndicators  a:nth-child(2)  span:nth-child(2)'))
-print(r.html.find('div#KeyIndicators > a:nth-child(2)')[0])
+#print(r.html.find('div#KeyIndicators > a:nth-child(2)')[0])
 # print(help(r))
 #print(r.html.find('div#middle tbody  tr  td  ul:nth-child(2)  div'))
 #middle > tbody > tr > td > ul:nth-child(2) > div > b > font
--- resquests.py
+++ resquests.py
@@ -1,5 +1,30 @@
 import requests
+from bs4 import BeautifulSoup
 
-#x = requests.get('https://w3schools.com/python/demopage.htm')
 x = requests.get('https://www.abs.gov.au/')
-print(x.text)
+#print(x)
+soup = BeautifulSoup(x.content, 'html.parser')
+
+#population
+population = soup.select('#KeyIndicators > a:nth-child(2)')
+
+#austrlian population live number
+print('population: ' + population[0].contents[4])
+
+#unemployment rate
+unemployment = soup.select('#KeyIndicators > a:nth-child(6)')
+print('unemployment rate: ' + unemployment[0].contents[4])
+
+#Average Weekly Earnings
+weeklyEarnings = soup.select('#KeyIndicators > a:nth-child(5)')
+print('average weekly earnings: ' + weeklyEarnings[0].contents[4])
+
+#consumer price index (CPI)
+consumerPriceIndex = soup.select('#KeyIndicators > a:nth-child(3)')
+print('CPI: ' +soup.select('#KeyIndicators > a:nth-child(3) > span:nth-child(3)')[0].contents[0]+ consumerPriceIndex[0].contents[4])
+
+#gross domestic product
+grossDomesticProduct = soup.select('#KeyIndicators > a:nth-child(4)')
+print('GDP: '+ soup.select('#KeyIndicators > a:nth-child(4) > span:nth-child(3)')[0].contents[0] + grossDomesticProduct[0].contents[4])
+
+
